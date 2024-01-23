4a1bd87d0b323698c239b5ec60ec3bc4ba31da62
Suresh R <121588527+gitSureshr20@users.noreply.github.com>
2024-01-23 14:45:00 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+let input = document.getElementById("input");
+let button = document.getElementById("add");
+let todoList = document.getElementById("todoList");
+
+let todos = [];
+window.onload = () => {
+  todos = JSON.parse(localStorage.getItem("todos")) || [];
+  todos.forEach((todo) => addtodo(todo));
+};
+button.addEventListener("click", () => {
+  todos.push(input.value);
+  localStorage.setItem("todos", JSON.stringify(todos));
+  addtodo(input.value);
+  input.value = "";
+});
+
+function addtodo(todo) {
+  let para = document.createElement("p");
+  para.innerText = todo;
+  para.style.userSelect = "none";
+  todoList.appendChild(para);
+
+  para.addEventListener("click", () => {
+    // para.style.textDecoration = "line-through";
+    // para.style.textDecoration = "line-through";
+    para.classList.toggle('checked');
+  });
+  para.addEventListener("dblclick", () => {
+    todoList.removeChild(para);
+    remove(todo);
+  });
+}
+
+function remove(todo) {
+  let index = todos.indexOf(todo);
+  if (index > -1) {
+    todos.splice(index, 1);
+  }
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
